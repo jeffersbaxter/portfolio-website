3e8db6f66ef104e6c16704eaba955c391648aafd
jeffersbaxter <jeffersbaxter@comcast.net>
2023-01-16 18:42:33 -0800

    Initial commit

---
+++ links.py
@@ -0,0 +1,62 @@
+links = [
+    {
+        'url': 'https://replit.com/@jeffersbaxter/Morse-Code#main.py',
+        'title': 'Morse Code',
+        'github': 'https://github.com/jeffersbaxter/morse-code',
+        'icon_classes': 'fa-solid fa-radio'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/restful-blog',
+        'title': 'Restful Blog',
+        'github': 'https://github.com/jeffersbaxter/restful-blog',
+        'icon_classes': 'fa-brands fa-microblog'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/amazon-price-tracker',
+        'title': 'Amazon Price Tracker',
+        'github': 'https://github.com/jeffersbaxter/amazon-price-tracker',
+        'icon_classes': 'fa-solid fa-tag'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/flight-deal-finder',
+        'title': 'Flight Deal Finder',
+        'github': 'https://github.com/jeffersbaxter/flight-deal-finder',
+        'icon_classes': 'fa-solid fa-plane'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/workout-tracker',
+        'title': 'Workout Tracker',
+        'github': 'https://github.com/jeffersbaxter/workout-tracker',
+        'icon_classes': 'fa-solid fa-dumbbell'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/book-review',
+        'title': 'Book Review',
+        'github': 'https://github.com/jeffersbaxter/book-review',
+        'icon_classes': 'fa-solid fa-book'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/100-must-see-movies',
+        'title': '100 Must See Movies',
+        'github': 'https://github.com/jeffersbaxter/100-must-see-movies',
+        'icon_classes': 'fa-solid fa-film'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/stock-news-alert',
+        'title': 'Stock News Alert',
+        'github': 'https://github.com/jeffersbaxter/stock-news-alert',
+        'icon_classes': 'fa-solid fa-arrow-trend-up'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/quiz-game',
+        'title': 'Quiz Game',
+        'github': 'https://github.com/jeffersbaxter/quiz-game',
+        'icon_classes': 'fa-solid fa-gamepad'
+    },
+    {
+        'url': 'https://github.com/jeffersbaxter/rain-alert',
+        'title': 'Rain Alert',
+        'github': 'https://github.com/jeffersbaxter/rain-alert',
+        'icon_classes': 'fa-solid fa-cloud-rain'
+    }
+]
---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from links import links
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', links=links)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
